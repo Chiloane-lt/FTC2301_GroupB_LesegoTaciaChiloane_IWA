2428b7139c3c5afb5e3b5e3b5c4dbcb7e09fa3d7
Chiloane-lt <chiloane.lt@hotmail.com>
2023-03-29 15:17:27 +0200

    Create files & add functionality.

---
+++ IWA_03/Challenge_2/scripts/alex.js
@@ -0,0 +1,6 @@
+const firstname = "Alex";
+const surname = "Pancakes";
+export const role = "Head of Marketing";
+
+const display= firstname + " " + surname + " " + role;
+document.querySelector('#alex').innerText = display;
--- IWA_03/Challenge_2/scripts/scripts.js
+++ IWA_03/Challenge_2/scripts/scripts.js
@@ -1,1 +1,6 @@
-console.log('Roles:', nwabisa.role, johanes.role, alex.role)
+import { role as johannes } from "/IWA_03/Challenge_2/scripts/johannes.js";
+import { role as nwabisa } from "/IWA_03/Challenge_2/scripts/nwabisa.js";
+import { role as alex } from "/IWA_03/Challenge_2/scripts/alex.js";
+
+// Added missing n to Johannes
+console.log('Roles:', nwabisa.role, johannes.role, alex.role);
