4df60aed9cefbd77742cb5c58a750c166801442c
Chiloane-lt <chiloane.lt@hotmail.com>
2023-03-29 12:22:42 +0200

    Update file structure and paths.

--- IWA_03/Challenge_1/scripts/scripts.js
+++ IWA_03/Challenge_1/scripts/scripts.js
@@ -1,5 +1,5 @@
-import { company } from '/IWA_03/scripts/configuration.js';
-import { year } from '/IWA_03/scripts/configuration.js';
+import { company } from '/IWA_03/Challenge_1/scripts/configuration.js';
+import { year } from '/IWA_03/Challenge_1/scripts/configuration.js';
 
 const message = '© ' + company + ' (' + year + ')';
 document.querySelector('footer').innerText = message;
