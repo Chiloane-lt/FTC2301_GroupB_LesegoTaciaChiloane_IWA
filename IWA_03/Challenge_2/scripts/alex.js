61781a4b6c9c4f9bc61ae80e9c61e6268107f7d9
Chiloane-lt <chiloane.lt@hotmail.com>
2023-03-29 17:41:22 +0200

    Add brackets.

--- IWA_03/Challenge_2/scripts/alex.js
+++ IWA_03/Challenge_2/scripts/alex.js
@@ -2,5 +2,5 @@ const firstname = "Alex";
 const surname = "Pancakes";
 export const role = "Head of Marketing";
 
-const display= firstname + " " + surname + " " + role;
+const display= firstname + " " + surname + " (" + role + ")";
 document.querySelector('#alex').innerText = display;
