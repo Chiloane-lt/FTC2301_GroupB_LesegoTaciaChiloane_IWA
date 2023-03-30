85a4794d0daf44ff6f0a513665d74dd3b84f41fa
Chiloane-lt <chiloane.lt@hotmail.com>
2023-03-30 11:20:57 +0200

    Add IWA_04 files.

--- IWA_03/Challenge_2/scripts/scripts.js
+++ IWA_03/Challenge_2/scripts/scripts.js
@@ -3,4 +3,4 @@ import { role as nwabisa } from "/IWA_03/Challenge_2/scripts/nwabisa.js";
 import { role as alex } from "/IWA_03/Challenge_2/scripts/alex.js";
 
 // Added missing n to Johannes
-console.log('Roles:', nwabisa.role, johannes.role, alex.role);
+console.log('Roles:', nwabisa, johannes, alex);
