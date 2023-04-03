93f2b9813427129e2dfaf6b2102671fbbe13cfb8
Chiloane-lt <chiloane.lt@hotmail.com>
2023-04-03 08:35:30 +0200

    Fix console error.

--- IWA_05/scripts/script.js
+++ IWA_05/scripts/script.js
@@ -1,17 +1,18 @@
 const FREE_WARNING = "Free shipping only applies to single customer orders";
-const BANNED_WARNING = "Unfortunately we do not ship to your country of residence";
+const BANNED_WARNING =
+  "Unfortunately we do not ship to your country of residence";
 const NONE_SELECTED = "0";
 
 // Set customer details initially since these are going to be used later in the script
 let customers = 1;
-let customerLocation = 'RSA';
+let customerLocation = "RSA";
 let currency = null;
 let shipping = 0;
 
-if (customerLocation === 'RSA') {
+if (customerLocation === "RSA") {
   shipping = 400;
   currency = "R";
-} else if (customerLocation === 'NAM') {
+} else if (customerLocation === "NAM") {
   shipping = 600;
   currency = "$";
 } else {
@@ -29,12 +30,15 @@ const totalNoShip = shoes + toys + shirts + batteries + pens;
 // shipping = null
 // currency = $
 
-if (customers === 1 && (customerLocation === 'NAM' || customerLocation === 'RSA')) {
+if (
+  customers === 1 &&
+  (customerLocation === "NAM" || customerLocation === "RSA")
+) {
   if (shoes + toys + batteries + pens + shirts >= 1000) {
-    if (customerLocation === 'NAM' && totalNoShip >= 60) {
+    if (customerLocation === "NAM" && totalNoShip >= 60) {
       shipping = 0;
     }
-    if (customerLocation === 'RSA' && totalNoShip >= 1000) {
+    if (customerLocation === "RSA" && totalNoShip >= 1000) {
       shipping = 0;
     }
   }
@@ -42,7 +46,7 @@ if (customers === 1 && (customerLocation === 'NAM' || customerLocation === 'RSA'
   console.log(FREE_WARNING);
 }
 
-if (customerLocation === 'NK') {
+if (customerLocation === "NK") {
   console.log(BANNED_WARNING);
 } else {
   console.log("price: ", currency, totalNoShip + shipping);
