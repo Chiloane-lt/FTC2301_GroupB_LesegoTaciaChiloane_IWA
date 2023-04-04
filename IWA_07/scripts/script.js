cd6418635ed87afa67ef85047901053a861087dd
Chiloane-lt <chiloane.lt@hotmail.com>
2023-04-04 14:04:27 +0200

    Complete challenge 1.

---
+++ IWA_07/scripts/script.js
@@ -0,0 +1,4 @@
+const value = 3;
+console.log(value + 4 + value);
+
+// 343 was logged because value was a character. Therefor using the "+" sign concatenated 3, 4 and 3. Resulting in 343. Changing data type of value fixed the problem as value will now be processed as an integer instead. 
