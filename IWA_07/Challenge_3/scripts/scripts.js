6d06cab8504ae43236c215bb775d87f483b87a9a
Chiloane-lt <chiloane.lt@hotmail.com>
2023-04-05 13:15:10 +0200

    Debug IWA_07.

---
+++ IWA_07/Challenge_3/scripts/scripts.js
@@ -0,0 +1,23 @@
+const leoName = 'Leo';
+const leoSurname = 'Musvaire';
+const leoBalance = '-9394';
+
+const sarahName = 'Sarah';
+const sarahSurname = 'Kleinhans';
+const sarahBalance = '-4582.21000111';
+
+const divider = '----------------------------------';
+
+// Only change below this line
+
+const owed = `R ${((parseInt(leoBalance) + parseInt(sarahBalance)) * -1).toFixed(2)}`;
+const leo = `${leoName} ${leoSurname} (Owed: R ${((leoBalance * -1).toFixed(2))})`;
+const sarah = `${sarahName} ${sarahSurname} (Owed: R ${((sarahBalance * -1).toFixed(2))})`;
+const total = "Total amount owed: ";
+const result = `${leo}
+${sarah}\n\n
+${divider}
+${total}${owed}
+${divider}`;
+
+console.log(result);
