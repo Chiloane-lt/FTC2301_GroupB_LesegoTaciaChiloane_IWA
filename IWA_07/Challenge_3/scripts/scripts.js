42f95223723ea8d647706c6fdedfdb8e30a85c91
Chiloane-lt <chiloane.lt@hotmail.com>
2023-04-06 09:19:21 +0200

    Remove trailing whitespacess.

--- IWA_07/Challenge_3/scripts/scripts.js
+++ IWA_07/Challenge_3/scripts/scripts.js
@@ -1,19 +1,21 @@
-const leoName = 'Leo';
-const leoSurname = 'Musvaire';
-const leoBalance = '-9394';
+const leoName = 'Leo'
+const leoSurname = 'Musvaire     '
+const leoBalance = '-9394'
 
-const sarahName = 'Sarah';
-const sarahSurname = 'Kleinhans';
-const sarahBalance = '-4582.21000111';
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.21000111'
 
 const divider = '----------------------------------';
 
 // Only change below this line
 
-const owed = `R ${((parseInt(leoBalance) + parseInt(sarahBalance)) * -1).toFixed(2)}`;
-const leo = `${leoName} ${leoSurname} (Owed: R ${((leoBalance * -1).toFixed(2))})`;
-const sarah = `${sarahName} ${sarahSurname} (Owed: R ${((sarahBalance * -1).toFixed(2))})`;
+const owed = `R ${((parseInt(leoBalance) + parseInt(sarahBalance)) * -1).toLocaleString()}`;
+const leo = `${leoName} ${leoSurname.trimEnd()} (Owed: R ${((leoBalance * -1).toFixed(2))})`;
+const sarah = `${sarahName.trimEnd()} ${sarahSurname} (Owed: R ${((sarahBalance * -1).toFixed(2))})`;
 const total = "Total amount owed: ";
 const result = `${leo}\n${sarah}\n\n${divider}\n${total}${owed}\n${divider}`;
 
 console.log(result);
+
+toLocaleString
