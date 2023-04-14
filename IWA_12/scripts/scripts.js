8b8c222a75e20cc806c0d6dff4433361248dac96
Chiloane-lt <chiloane.lt@hotmail.com>
2023-04-14 16:42:03 +0200

    Edit book 1 styles.

--- IWA_12/scripts/scripts.js
+++ IWA_12/scripts/scripts.js
@@ -27,10 +27,11 @@ const STATUS_MAP = {
 
 // Edit below line 
 
-const status = document.querySelector();
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
+const book0 = document.querySelector('#book1');
+const status0 = book0.querySelector('.status').innerHTML;
+const reserve0 = book0.querySelector('.reserve');
+const checkout0 = book0.querySelector('.checkout');
+const checkin0 = book0.querySelector('.checkin');
 
 // status = selector(status)
 // reserve = selector(reserve)
@@ -42,11 +43,15 @@ checkin = selector(checkin)
 // checkout = selector(checkout)
 // checkin = selector(checkin)
 
-// checkin.0.color = none
-// status.0.style.color = STATUS_MAP.status.color
-// reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-// checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-// checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+checkin0.style.color = '';
+book0.querySelector('.status').style.color = STATUS_MAP[status0].color;
+reserve0.innerHTML = STATUS_MAP[status0].canReserve ? 'enabled' : 'disabled';
+checkout0.innerHTML = STATUS_MAP[status0].canCheckout ? 'enabled' : 'disabled';
+checkin0.innerHTML = STATUS_MAP[status0].canCheckIn ? 'enabled' : 'disabled';
+
+console.log(status0);
+console.log(STATUS_MAP[status0].canReserve);
+
 
 // checkin.1.color = none
 // status.1.style.color = STATUS_MAP.status.color
